42cfd2dd508591dc305c6bd18665bd6c8a4081b1
AlexDelei <alexdelei125@gmail.com>
2023-09-22 15:48:19 +0300

    shell test

--- shell.c
+++ shell.c
@@ -1,50 +1,50 @@
 #include "main.h"
-/**
- * main - This is a simple shell created by
- * Marisol Ramirez Henao and David Alejandro Hincapié
- * for Holberton School
- * Return: 0 if success
- */
+
 int main(void)
 {
-	ssize_t bytes_rd = 0; /** Bytes leídos de un getline*/
-	size_t bf_size = 0; /**Tamaño del buffer*/
-	char *entry = NULL, *arguments[20]; /**String de args que ingresa el usr*/
-	int counter = 1, vf_stat = 0, exist_stat = 0, exit_stat = 0, blt_stat = 0;
+    ssize_t bytes_read = 0; 
+    size_t buffer_size = 0;
+    char *entry = NULL, *arguments[20];
+    int counter = 1, exit_status = 0;
 
-	_printp("$ ", 2);/**prompt mini-shell*/
-	bytes_rd = getline(&entry, &bf_size, stdin); /**sizeof entry, o -1 (EOF))*/
-	while (bytes_rd != -1)
-	{
-		if (*entry != '\n')
-		{
-			fill_args(entry, arguments);
-			if (arguments[0] != NULL)
-			{
-				exist_stat = exist(arguments[0]);/**Exist evalua si path ingresado existe*/
-				if (exist_stat != 0)/**No encontró el archivo*/
-				{
-					vf_stat = verify_path(arguments);
-					if (vf_stat == 0)
-						exit_stat = exec(arguments), free(entry), free(*arguments);
-					else
-					{
-					blt_stat = verify_blt(arguments, exit_stat);
-					if (blt_stat != 0)
-						exit_stat = print_not_found(arguments, counter), free(entry);
-					}
-				}
-				else /**Encontró el archivo*/
-					exit_stat = exec(arguments), free(entry);
-			}
-			else
-				free(entry);
-		}
-		else if (*entry == '\n')
-			free(entry);
-		entry = NULL, counter++;
-		_printp("$ ", 2), bytes_rd = getline(&entry, &bf_size, stdin);
-	}
-	last_free(entry);
-	return (exit_stat);
+    while (1)
+    {
+        _printp("$ ", 2);
+        bytes_read = getline(&entry, &buffer_size, stdin);
+        if (bytes_read == -1)
+            break;
+
+        if (*entry != '\n')
+        {
+            fill_args(entry, arguments);
+            if (arguments[0] != NULL)
+            {
+                int exist_status = exist(arguments[0]);
+                if (exist_status != 0)
+                {
+                    int verify_status = verify_path(arguments);
+                    if (verify_status == 0)
+                        exit_status = exec(arguments), free(entry);
+                    else
+                    {
+                        int builtin_status = verify_blt(arguments, exit_status);
+                        if (builtin_status != 0)
+                            exit_status = print_not_found(arguments, counter), free(entry);
+                    }
+                }
+                else
+                    exit_status = exec(arguments), free(entry);
+            }
+            else
+                free(entry);
+        }
+        else
+            free(entry);
+
+        entry = NULL;
+        counter++;
+    }
+
+    last_free(entry);
+    return exit_status;
 }
